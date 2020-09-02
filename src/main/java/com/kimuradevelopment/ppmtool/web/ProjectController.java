7d0b109b2378d551d9b8a6172c94594830fc456a
Scott Reyell <smreyell@gmail.com>
2020-09-01 22:00:10 -0700

    ValidationErrorServiceMap

--- src/main/java/com/kimuradevelopment/ppmtool/web/ProjectController.java
+++ src/main/java/com/kimuradevelopment/ppmtool/web/ProjectController.java
@@ -2,13 +2,23 @@ package com.kimuradevelopment.ppmtool.web;
 
 import com.kimuradevelopment.ppmtool.domain.Project;
 import com.kimuradevelopment.ppmtool.service.ProjectService;
+import com.kimuradevelopment.ppmtool.service.ValidationErrorServiceMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -17,8 +27,15 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    ValidationErrorServiceMap validationErrorServiceMap;
+
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+        ResponseEntity<?> errorMap = validationErrorServiceMap.ValidationErrorServiceMap(result);
+
+        if(errorMap != null) return errorMap;
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
